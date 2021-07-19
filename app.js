f72a3cd3055635acc2cce88e69e87b4e22cf77a2
Julia K Amadio <jkahvedjian@gmail.com>
2021-07-19 19:18:07 -0300

    Some Global Variables

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const amount = 19
+// GLOBALS - NO WINDOWS
 
-if (amount < 10) {
-	console.log(`That's a small number`);
-}
-else {
-	console.log(`That's a large number`);
-}
+// __dirname	- path to current directory
+// __filename	- file name
+// require		- function to use modules (Common JS)
+// module		- info about current module (file)
+// process		- info about env where the program is being executed
 
-console.log(`hey it's my first node app!`);
+console.log("__dirname");
+console.log(__dirname);
+console.log('\n\n\n\n///////////////////-------------------------/////////////////////\n\n\n\n\n');
+console.log("__filename");
+console.log(__filename);
+console.log('\n\n\n\n///////////////////-------------------------/////////////////////\n\n\n\n\n');
+console.log("require");
+console.log(require);
+console.log('\n\n\n\n///////////////////-------------------------/////////////////////\n\n\n\n\n');
+console.log("module");
+console.log(module);
+console.log('\n\n\n\n///////////////////-------------------------/////////////////////\n\n\n\n\n');
+console.log("process");
+console.log(process);
+console.log('\n\n\n\n///////////////////-------------------------/////////////////////\n\n\n\n\n');
+
+setInterval(() => {
+	console.log('hello');
+}, 1000);
