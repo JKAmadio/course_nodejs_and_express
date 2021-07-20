0f4a3227319a2233d639d7b233e1cbf65142cdf2
Julia K Amadio <jkahvedjian@gmail.com>
2021-07-20 01:24:26 -0300

    Event Loop - setTimeout

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-const { readFile } = require('fs');
-
-console.log('started first task');
-readFile('./content/first.txt', 'utf-8', (err, result) => {
-	if(err) {
-		console.log(err);
-		return;
-	}
-	console.log(result);
-	console.log('completed first task');
-})
-console.log('next task');
+// started operating system process
+console.log('first');
+setTimeout(() => {
+	console.log('second')
+}, 0);
+console.log('third');
+// completed and exited operating system process
