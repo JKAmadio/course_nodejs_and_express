99932898f4ed63b50e6e93aaed17294153422574
Julia K Amadio <jkahvedjian@gmail.com>
2021-07-20 01:01:41 -0300

    Node Package Manager

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const http = require('http');
+// npm - global command, comes with node
+// npm --version
 
-// req = client request | res = server response
-const server = http.createServer((req, res) => {
-	if(req.url === '/') {
-		res.end('Welcome to out home page');
-	}
-	if(req.url === '/about') {
-		res.end('This is our short history');
-	}
-	else {
-		res.end(`
-			<h1>Ooops!</h1>
-			<p>we can't seem to find the page you're looking for</p>
-			<a href="/">Back Home</a>
-		`);
-	}
-});
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000);
+// global dependency - use it in any project
+// npm install -g <packageName>
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+// npm uninstall <packageName> (remove the package  selected)
